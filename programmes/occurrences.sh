fd65c42fd2fd56faeed87a48d1727f271bd796f5
Lise-Brisset <lise.brisset9@gmail.com>
2023-11-22 11:46:20 +0100

    ajout script compter occurences de notre mot

---
+++ programmes/occurrences.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+langue=$1
+lineno=1
+FICHIER="../dumps-text/$1-$lineno.txt"
+
+#Si le premier argument n'est pas un fichier
+if [ $# -ne 1 ]
+then
+    echo "un argument attendu exactement"
+    exit 1
+fi
+
+cat "$FICHIER" | grep -P -o "\p{Latin}+" | tr "[:upper:]" "[:lower:]" | tr "É" "é" | sort | uniq -c | grep -P "santé"

c5395a32d91041ce8ac7aee625df3766ca8dc293
Lise-Brisset <lise.brisset9@gmail.com>
2023-11-22 11:44:45 +0100

    maj dump.sh

--- programmes/dump.sh
+++ programmes/dump.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+#lignes permettant la vérifications des arguments : 
+if [ $# -ne 2 ]
+then
+	echo "deux arguments attendus exactement"
+	exit
+else
+
+if [ ! -f ../URLs/$URLS ]
+	then
+		echo "le fichier n'existe pas"
+		exit
+	fi
+fi
+
 langue=$1
 URLS=$2
 lineno=1
